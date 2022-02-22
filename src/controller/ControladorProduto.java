ced56e660c0985f5cd8dc9afd56874d0dd7d6f11
karolyne <karoldm1579@gmail.com>
2022-02-22 12:45:59 -0300

    fix cadastro de produto

--- src/controller/ControladorProduto.java
+++ src/controller/ControladorProduto.java
@@ -3,6 +3,7 @@ package controller;
 import factory.FactoryProduto;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Iterator;
 import model.Fornecedor;
 import model.LojaDepartamento;
 import model.Produto;
@@ -43,7 +44,7 @@ public class ControladorProduto {
     
     public Fornecedor getFornecedorByNome(String nome){
         for(Fornecedor f: LojaDepartamento.getFornecedores()){
-            if(f.getNome() == nome) return f;
+            if(f.getNome().equals(nome)) return f;
         }
         return null;
     }
@@ -94,5 +95,28 @@ public class ControladorProduto {
             if(p.getCodigoProduto() == codigo) return p;
         }
         return null;
+    }
+    
+    public Object[][] relatorioProdutosCadastrados(){
+        ArrayList<Produto> produtos = LojaDepartamento.getProdutos();
+        
+        Object[][] produtosDados = new Object[7][produtos.size()];
+        
+        Iterator<Produto> iterator = produtos.iterator();
+        
+        int i = 0;
+        while(iterator.hasNext()){
+            Produto p = iterator.next();
+            produtosDados[0][i] = p.getCodigoProduto();
+            produtosDados[1][i] = p.getNome();
+            produtosDados[2][i] = p.getDescricao();
+            produtosDados[3][i] = p.getDataFabricacao();
+            produtosDados[4][i] = p.getValor();
+            produtosDados[5][i] = p.getFornecedor().getNome();
+            produtosDados[6][i] = p.isDisponivel();
+            i++;
+        }
+        
+        return produtosDados;
     }
 }
